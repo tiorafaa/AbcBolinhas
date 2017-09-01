1b26fe68afe96c49a9914e4e9047e310c701e3f6
Rafael Salgueiroza <tiorafa2005@gmail.com>
2017-09-01 19:33:22 -0300

    Criadas as classes Orçamento e Imposto

---
+++ AbcBolinhas/src/abcbolinhas/Imposto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abcbolinhas;
+
+/**
+ *
+ * @author tiora
+ */
+public class Imposto {
+
+    public Imposto() {
+    }
+
+    public double calcula(Orcamento orcamento) {
+        System.out.println("teste");
+        return orcamento.getValor() * 0.1;
+    }
+}
---
+++ AbcBolinhas/src/abcbolinhas/Orcamento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tiora
+ */
+public class Orcamento {
+
+    private double valor;
+
+    public Orcamento(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
